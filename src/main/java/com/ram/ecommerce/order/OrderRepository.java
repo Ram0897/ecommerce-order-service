7d00fe2335f6f046777560a61e943a61eee8c0d2
Ram0897 <rapoluram88@gmail.com>
2026-09-11 19:58:06 +0530

    feat: add order repository

---
+++ src/main/java/com/ram/ecommerce/order/OrderRepository.java
@@ -0,0 +1,4 @@
+package com.ram.ecommerce.order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface OrderRepository extends JpaRepository<Order,Long>{Optional<Order> findByIdempotencyKey(String key);}
