db8b5fde10010a048b0bf0a494b26298d662f844
Ram0897 <rapoluram88@gmail.com>
2026-09-11 19:57:16 +0530

    feat: add cart repository

---
+++ src/main/java/com/ram/ecommerce/cart/CartRepository.java
@@ -0,0 +1,4 @@
+package com.ram.ecommerce.cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface CartRepository extends JpaRepository<Cart,Long>{ Optional<Cart> findByUserId(String userId); }
